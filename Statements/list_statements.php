134065507fbd3a1214bd47bea2eff89df55258f8
Tim <99051246@mydavinci.nl>
2020-05-08 14:25:14 +0200

    display list

--- Statements/list_statements.php
+++ Statements/list_statements.php
@@ -1,10 +1,21 @@
 <?php
 
+function GetLists($conn){
+  $stmt = "SELECT * FROM `lists`";
+  $query = $conn->prepare($stmt);
+  $query->execute();
+  $result = $query->fetchAll();
+
+  return $result;
+
+  $conn = NULL;
+}
+
 function AddList($conn, $name){
   $stmt = "INSERT INTO `lists` SET name = :name";
   $query = $conn->prepare($stmt);
   $query->bindParam(":name", $name);
   $query->execute();
 
-  $conn = null;
+  $conn = NULL;
 }
--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <?php require('connection.php'); ?>
 <?php require('Statements/list_statements.php');?>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
     <title>To-Do List</title>
 </head>
-<body>
-<div class="h-100 jumbotron">
-  <h1 class="display-4">To-Do List</h1>
-  <hr class="my-4">
-  <p>Lists:</p>
-  <hr class="my-4">
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#List-Add-Modal">
-  Add List
-  </button>
-  <?php require('Modals/add_list_modal.php'); 
-  
-  ?>
 
+<body>
+    <div class="h-100 jumbotron">
+        <h1 class="display-4">To-Do List</h1>
+        <hr class="my-4">
+        <?php require('Templates/tabels.php') ?>
+        <hr class="my-4">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#List-Add-Modal">
+            Add List
+        </button>
+        <?php require('Modals/add_list_modal.php'); ?>
+    </div>
 </body>
+
 </html>
